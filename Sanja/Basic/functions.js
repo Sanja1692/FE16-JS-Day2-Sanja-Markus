8d221d5ebb93cc82180b441e1dc74500880ef8e6
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-05-30 11:04:03 +0200

    ex1

---
+++ Sanja/Basic/functions.js
@@ -0,0 +1,5 @@
+function crystalGazer(childrenNum, parentName, location, jobTitle) {
+    let result = "You will be a $(jobTitle) in $(location) and married to $(partnerName) with $(childrenNum).";
+    console.log(result);
+}
+crystalGazer("3", "Doe", "New York", "Develover");

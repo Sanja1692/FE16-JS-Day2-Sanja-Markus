786e7045f353161ffab76dbae62c90dd8f412cc0
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-05-30 14:08:36 +0200

    seconds

---
+++ Sanja/Adv/functions.js
@@ -0,0 +1,27 @@
+//Time Convert
+function convert(minutes) {
+    const hour = Math.floor(minutes / 60);
+    return [hour, minutes];
+}
+console.log(convert(90))
+
+// function timeConvert(minutes) {
+//     hours = Math.floor(minutes / 60);
+//     restOfMinutes = minutes % 60;
+//     return (minutes + " minutes" + " = " + hours + " hour(s) and " + restOfMinutes + " minutes.");
+//   }
+
+//   console.log(timeConvert(200));
+
+//Seconds
+function calcTime(totalSeconds) {
+    let week = Math.floor(totalSeconds / 604800);
+    let day = Math.floor((totalSeconds % 604800) / 86400);
+    let hour = Math.floor(((totalSeconds % 604800) % 86400) / 3600);
+    let min = Math.floor((((totalSeconds % 604800) % 86400) % 3600) / 60);
+    let sec = Math.floor((((totalSeconds % 604800) % 86400) % 3600) % 60);
+    let result = [week, day, hour, min, sec];
+    console.log(totalSeconds + " seconds = week(s): " + week + ", day(s): " + day + ", hour(s): " + hour + ", minute(s): " + min + ", second(s): " + sec);
+    return result;
+}
+calcTime(51561651);

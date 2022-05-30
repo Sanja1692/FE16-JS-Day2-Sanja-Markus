f57d6699b5c92def3314e23d96ed583729347500
Desktop-PC <agermarkus123@gmail.com>
2022-05-30 10:54:53 +0200

    basic

---
+++ Markus/Basic/index.js
@@ -0,0 +1,30 @@
+function crystalGazer(NumOfChildren, PartnerName, Location, JobTitle) {
+    console.log("You will be a " + JobTitle + " in " + Location + " and married to " + PartnerName + " with " + NumOfChildren + " children.")
+}
+
+function ageCalculator(birthYear) {
+    var year = new Date().getFullYear();
+    var age = year - birthYear
+    console.log("You are either " + age + " or " + (age-1))
+}
+
+function DegreesToRadians(degrees) {
+    Radiant = degrees * (Math.PI/180)
+    console.log(Radiant)
+}
+
+function Box (width, height, depth) {
+    area = width * height;
+    volume = width * height * depth;
+
+    var result = [area, volume];
+    return result;
+}
+
+
+
+crystalGazer(2, "Anna", "Barcelona", "Full Stack Developer")
+ageCalculator(1998, 2022)
+DegreesToRadians(90)
+var box = Box(2,7,5)
+console.log("the area of the box is: " + box[0] + "\n" + "The volume of the box is: " + box[1])
